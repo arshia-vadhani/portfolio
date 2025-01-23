21e03195a6da9b62520b673273789785f4fbd1c3
arshia-vadhani <130110649+arshia-vadhani@users.noreply.github.com>
2025-01-23 15:07:03 -0800

    changes

--- global.js
+++ global.js
@@ -23,7 +23,10 @@ let nav = document.createElement('nav');
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
     }
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
 if (!ARE_WE_HOME && !url.startsWith('http')) {
