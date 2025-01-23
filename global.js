9b03f6b0ad8e7cf75e8bd61ab5f8287ba8aab069
arshia-vadhani <130110649+arshia-vadhani@users.noreply.github.com>
2025-01-23 14:06:47 -0800

    trying to update things

--- global.js
+++ global.js
@@ -3,3 +3,23 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const navLinks = $$("nav a");
+console.log(navLinks);
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+  );
+currentLink?.classList.add('current');
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    // add the rest of your pages here
+  ];
+let nav = document.createElement('nav');
+  document.body.prepend(nav);
+  for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // TODO create link and add it to nav
+  }
+nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
