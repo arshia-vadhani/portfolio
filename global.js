6fc16edfabfa32d8ea7916f9d8c699d11d9a0d2d
arshia-vadhani <130110649+arshia-vadhani@users.noreply.github.com>
2025-01-24 17:47:06 -0800

    few changes

--- global.js
+++ global.js
@@ -19,7 +19,7 @@ let pages = [
 
   ];
 let nav = document.createElement('nav');
-  document.body.prepend(nav);
+document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
