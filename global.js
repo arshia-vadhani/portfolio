d91e48e6b648babf364c94359e21e0f0fa0b8c16
arshia-vadhani <130110649+arshia-vadhani@users.noreply.github.com>
2025-01-25 17:07:02 -0800

    Revert "so so many changes"
    This reverts commit 845efa2efa6ab9a7858dd62a0b262bdd0445dd13.

--- global.js
+++ global.js
@@ -32,22 +32,24 @@ select.addEventListener('input', function (event) {
   console.log('Color scheme changed to:', newColorScheme);
 });
 
-let nav = document.createElement('nav');
-document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
 let pages = [
     { url: '', title: 'Home' },
-    { url: '/projects/', title: 'Projects' },
-    { url: '/contact/', title: 'Contact' },
-    { url: '/CV/', title: 'CV'},
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'CV/', title: 'CV'},
     { url: 'https://github.com/arshia-vadhani', title: 'Github' }
 
   ];
-const ARE_WE_HOME = document.documentElement.classList.contains('home');
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
   
-  url = !ARE_WE_HOME && !p.external ? '../${url}' : url;
+  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
