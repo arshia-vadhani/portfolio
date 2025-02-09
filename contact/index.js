fe94711df0e319d1bd9facb9cc7ec026738e6136
arshia-vadhani <130110649+arshia-vadhani@users.noreply.github.com>
2025-02-08 21:08:26 -0800

    Update index.js

--- contact/index.js
+++ contact/index.js
@@ -1,22 +1,6 @@
 import { fetchJSON, renderProjects } from './global.js';
+const projects = await fetchJSON('./lib/projects.json'); // Select the container for displaying projects
+const latestProjects = projects.slice(0, 3);
+const projectsContainer = document.querySelector('.projects');
+renderProjects(latestProjects, projectsContainer, 'h2');
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const projects = await fetchJSON('./projects/projects.json'); // Select the container for displaying projects
-    try {
-    // Fetch the project data from projects.json located in the lib folder
-        const projects = await fetchJSON('.//projects.json'); 
-
-        // Check if the projects data is valid
-        if (projects && projects.length > 0) {
-            // Filter the first 3 projects from the fetched data
-            const latestProjects = projects.slice(0, 3);
-
-            // Use the renderProjects function to display the filtered projects
-            renderProjects(latestProjects, projectsContainer, 'h2');
-        } else {
-            console.error('No projects found in the data.');
-        }
-  } catch (error) {
-    console.error('Error fetching projects:', error);
-  }
-});
