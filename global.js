1ed26117a9dbf6d4d25b824999462968f6997cf6
arshia-vadhani <130110649+arshia-vadhani@users.noreply.github.com>
2025-02-08 18:46:27 -0800

    Update global.js

--- global.js
+++ global.js
@@ -29,39 +29,40 @@ select.addEventListener('input', function (event) {
   console.log('Color scheme changed to:', newColorScheme);
 });
 
-// Navigation code starts here
-const ARE_WE_HOME = document.documentElement.classList.contains('home');
+document.addEventListener('DOMContentLoaded', function () {
+  const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
-let pages = [
-    { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact' },
-    { url: 'CV/', title: 'CV'},
-    { url: 'https://github.com/arshia-vadhani', title: 'Github', external: true }
-];
+  let pages = [
+      { url: '', title: 'Home' },
+      { url: 'projects/', title: 'Projects' },
+      { url: 'contact/', title: 'Contact' },
+      { url: 'CV/', title: 'CV'},
+      { url: 'https://github.com/arshia-vadhani', title: 'Github', external: true }
+  ];
 
-let nav = document.createElement('nav');
-document.body.prepend(nav);
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
 
-for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
-  
-  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-  
-  let a = document.createElement('a');
-  a.href = url;
-  a.textContent = title;
-  a.classList.toggle(
-    'current',
-    a.host === location.host && a.pathname === location.pathname
-  );
-  if (p.external) {
-    a.target = "_blank";
-    a.rel = "noopener noreferrer";
+  for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    a.classList.toggle(
+      'current',
+      a.host === location.host && a.pathname === location.pathname
+    );
+    if (p.external) {
+      a.target = "_blank";
+      a.rel = "noopener noreferrer";
+    }
+    nav.append(a);
   }
-  nav.append(a);
-}
+});
 
 export async function fetchJSON(url) {
   try {
