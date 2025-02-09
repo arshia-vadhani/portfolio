581acaeab38df4798267f214c8f68324bbcf24fa
arshia-vadhani <130110649+arshia-vadhani@users.noreply.github.com>
2025-02-08 21:18:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { fetchJSON, renderProjects } from './global.js';
 const projects = await fetchJSON('./lib/projects.json'); // Select the container for displaying projects
 const latestProjects = projects.slice(0, 3);
-const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
